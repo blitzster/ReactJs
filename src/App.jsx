0e9abe4663713b059e0e2e165a39b51491ada1ae
Akshay S <blitz122333@gmail.com>
2024-07-01 18:07:44 +0530

    added button

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@ import Header from './Header.jsx'
 import Footer from './Footer.jsx'
 import Food from './Food.jsx'
 import Card from './Card.jsx'
+import Button from './Button.jsx'
 
 function App() {
   return (
-    <Card>
-    </Card>
+    // <Card>
+    // </Card>
+
+    <Button>
+
+    </Button>
   );
 }
 

efbbb33f1ba113eb0aa7b33a7e49300a68ba3cfa
Akshay S <blitz122333@gmail.com>
2024-07-02 22:04:02 +0530

    props

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,19 @@ import Footer from './Footer.jsx'
 import Food from './Food.jsx'
 import Card from './Card.jsx'
 import Button from './Button.jsx'
+import Student from './Student.jsx'
 
 function App() {
   return (
     // <Card>
     // </Card>
+    <>
+      <Student name="Spongbob" age={30} isStudent={true} />
+      <Student name="Patrick" age={42} isStudent={false} />
+      {/* <Button>
 
-    <Button>
-
-    </Button>
+      </Button> */}
+    </>
   );
 }
 
